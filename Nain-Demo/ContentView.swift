0a85fb34c2e1e50cb42b28e4f0beecfd9e6468ee
ruofan <ruofan.srf@antgroup.com>
2023-03-22 11:07:10 +0800

    Initial Commit

---
+++ Nain-Demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Nain-Demo
+//
+//  Created by ruofan on 2023/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nain-Demo/Nain_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nain_DemoApp.swift
+//  Nain-Demo
+//
+//  Created by ruofan on 2023/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Nain_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
